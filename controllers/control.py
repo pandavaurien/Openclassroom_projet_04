222ff21d4d7f2c3c1f583a5c108ca07cbc2ea170
Alexandre Jurquet <a.jurquet@gmail.com>
2021-04-21 12:20:55 +0200

    Repertoires et fichiers vides

--- controllers/control.py
+++ controllers/control.py
@@ -1,7 +1,4 @@
-from views import view_main
-from views import menus
-from views import player
-from controllers import control_player
+
 
 
 class ApplicationController:
@@ -10,56 +7,3 @@ class ApplicationController:
         self.controller = HomeMenuController()
         self.controller.run()
 
-
-class HomeMenuController:
-    view = view_main.MainMenuDisplay()
-    get_user_choice = view_main.GetUserChoiceInMenu()
-    display_menu = view_main.DisplayMenu()
-    menu = menus.Menus()
-    
-    def run(self):
-        # Show the main title
-        self.view.display_title() 
-
-        # Show the main menu
-        self.display_menu(self.menu.main_menu_dict) 
-
-        # Ask and return the user's choice
-        entry = self.get_user_choice(self.menu.main_menu_dict)  
-        
-        self.link_to_menu(entry)
-
-    def link_to_menu(self, entry): 
-        self.player_menu_controller = PlayerMenuController()
-        self.tournament_menu_controller = TournamentMenuController()
-        self.quit_app_controller = QuitAppController()
-        self.report_menu_controller = ReportMenuController()
-        
-        if entry == "1":
-            self.player_menu_controller.run()
-        if entry == "2":
-            self.tournament_menu_controller.run()
-        if entry == "3":
-            self.report_menu_controller.run()
-        if entry == "4":
-            self.quit_app_controller.run()
-
-
-
-class TournamentMenuController:
-    display_menu = view_main.DisplayMenu()
-    menu = menus.Menus()
-    def run(self):
-        self.display_menu(self.menu.tournament_root_menu_dict)
-
-
-class ReportMenuController:
-    display_menu = view_main.DisplayMenu()
-    menu = menus.Menus()
-    def run(self):
-        self.display_menu(self.menu.report_root_menu_dict)
-
-
-class QuitAppController:
-    def run(self):
-        pass
--- views/view_main.py
+++ views/view_main.py
@@ -13,24 +13,3 @@ class MainMenuDisplay:
               " Entrez le numéro correspondant au menu choisi :\n"
               "------------------------------------------------\n")             
 
-
-class DisplayMenu:
-    """Display the menu choosen by the user"""
-    def __call__(self, menu_dict):
-        for key in menu_dict:
-            print(f"{key} : {menu_dict[key]}")
-        print()
-
-
-class GetUserChoiceInMenu:
-    """Ask user to choose in a menu"""
-    def __call__(self, menu_dict_choosen):
-        valid_choice = False
-        while not valid_choice:
-            choice = input("--> ")
-            if choice in menu_dict_choosen:
-                valid_choice = True
-                print(menu_dict_choosen[choice])
-            else:
-                print("Vous devez entrer le chiffre correspondant au menu")
-        return choice
